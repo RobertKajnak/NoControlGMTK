35d768c5c40023d8e69e1cae636e07e3fa7d6d27
RobertKajnak <hezirisz@gmail.com>
2020-07-11 14:15:41 +0900

    Created a main menu stub

---
+++ Interface/MainMenu.gd
@@ -0,0 +1,33 @@
+extends Control
+
+
+func _ready():
+	for control in $CenterContainer/MainContainer/ButtonContainer.get_children():
+		if "Button" in control.name:
+			control.connect("pressed",self,"_on_ButtonStart_pressed",[control.scene_to_load])
+
+#%% Interface handling
+func _on_ButtonStart_pressed(to_load):
+	if to_load == 'exit':
+		get_tree().quit()
+	elif to_load == 'Options':
+		$"/root/Global".control_level += 1
+		print($"/root/Global".control_level)
+		return
+		
+	$CenterContainer/MainContainer/ButtonContainer/ButtonOptions/Label.text = str($"/root/Global".control_level)
+	#var _err = get_tree().change_scene(to_load)
+	
+#%% Input handling
+func _unhandled_input(event):
+	if event is InputEventKey:
+		if event.pressed and event.scancode == KEY_ESCAPE:
+			get_tree().quit()
+			
+func _notification(what):
+	if what == MainLoop.NOTIFICATION_WM_QUIT_REQUEST:
+		# For Windows
+		pass        
+	if what == MainLoop.NOTIFICATION_WM_GO_BACK_REQUEST: 
+		# For android
+		get_tree().quit()
