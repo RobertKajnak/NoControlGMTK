f8705e53aa2932ac3415943e23fea54f6e565e4d
KovaxG <georgesmth202@gmail.com>
2020-07-12 21:53:01 +0300

    Added command queue

--- src/scenes/NothingButton.gd
+++ src/scenes/NothingButton.gd
@@ -1,4 +1,14 @@
 extends Button
 
+var server_time = 0
+var times = []
+
 func _on_NothingButton_button_down():
-	Global.action_state = "Nothing"
+	times.append(server_time)
+	
+func _process(delta):
+	print(server_time)
+	server_time = server_time + delta
+	for time in times:
+		if server_time - time > 10:
+			Global.action_state = "Nothing"
