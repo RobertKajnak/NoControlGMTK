e22f03fb3ef651e575d035b6846d413a780ceb56
KovaxG <georgesmth202@gmail.com>
2020-07-12 22:11:11 +0300

    Timeout fit

--- src/scenes/PlantButton.gd
+++ src/scenes/PlantButton.gd
@@ -7,8 +7,8 @@ func _on_PlantButton_button_down():
 	times.append(server_time)
 	
 func _process(delta):
-	print(server_time)
 	server_time = server_time + delta
 	for time in times:
 		if server_time - time > 10:
+			times.erase(time)
 			Global.action_state = "Plant"
